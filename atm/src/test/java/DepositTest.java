6e6b9d23100ad1c4f02670192deacafdae4b96fb
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-05-27 09:17:13 -0500

    Unit testing for Deposit

--- atm/src/test/java/DepositTest.java
+++ atm/src/test/java/DepositTest.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import static org.mockito.Mockito.*;
 
 public class DepositTest {
@@ -14,9 +18,12 @@ public class DepositTest {
     private DepositSlot depositSlot;
     private Keypad keypad;
     private Deposit deposit;
-
+    private ScheduledExecutorService scheduler;
     @Before
     public void setUp() {
+
+        scheduler = Executors.newScheduledThreadPool(1);
+
         screen = mock(Screen.class);
         bankDatabase = mock(BankDatabase.class);
         depositSlot = mock(DepositSlot.class);
@@ -26,18 +33,55 @@ public class DepositTest {
     }
 
     @Test
-    public void testDepositTransaction() {
-        // Configurando comportamiento esperado para el método promptForDepositAmount
+    public void testMessageToDeposit() {
+
+        when(keypad.getInput()).thenReturn(1000); // Simulando un depósito de $10.00
+
+        // Ejecutar el método bajo prueba
+        deposit.execute();
+        // probamos si muestra un mensaje para agregar el monto del depósito
+        verify(screen).displayMessage("\nPlease insert a deposit envelope containing ");
+    }
+
+    @Test
+    public void testEnterDeposit() {
+        // Simulamos el ingreso de un número sin punto decimal y que luego se muestre en $
+
         when(keypad.getInput()).thenReturn(1000); // Simulando un depósito de $10.00
 
         // Ejecutar el método bajo prueba
         deposit.execute();
 
-        // Verificar que se llamaron a los métodos adecuados en el objeto screen
-        verify(screen).displayMessage("\nPlease insert a deposit envelope containing ");
+        // Verificar que si se muestre en dolares
         verify(screen).dispalyDollarAmount(10.00);
-        verify(screen).displayMessageLine(".");
 
+
+    }
+    @Test
+    public void testCancelDeposit() {
+        // Simulamos el comportamiento de la función
+
+        when(keypad.getInput()).thenReturn(0); // Simulando un 0 de cancelacíon
+
+        // Ejecutar el método bajo prueba
+        deposit.execute();
+
+        // Verificar que si muestra el mensaje de cancelación
+        verify(screen).displayMessageLine("\nCanceling transaction...");
+
+
+    }
+    @Test
+    public void testCancelDueToInactivity() throws InterruptedException {
+        when(keypad.getInput()).thenReturn(1000); // Simulando un depósito de $10.00
+
+        deposit.execute();
+
+        // Simulamos la inactividad esperando un tiempo mayor a los 2 minutos
+        scheduler.awaitTermination(2, TimeUnit.MINUTES);
+
+        verify(screen).displayMessage("\nPlease enter a deposit amount in CENTS (or 0 to cancel): ");
+        verify(screen).displayMessageLine("\nYou did not insert an envelope, so the ATM has canceled your transaction.");
 
     }
 }
